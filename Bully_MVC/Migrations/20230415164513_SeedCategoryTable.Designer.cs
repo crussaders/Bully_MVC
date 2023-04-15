256b2f376475cbc9c758d7c63ceac49b0264b582
Manish Kumar Pandey <896849@cognizant.com>
2023-04-15 22:18:14 +0530

    Adding dummy data for category list and seeding data in database

--- Bully_MVC/Migrations/20230415164513_SeedCategoryTable.Designer.cs
+++ Bully_MVC/Migrations/20230415164513_SeedCategoryTable.Designer.cs
@@ -3,6 +3,7 @@ using Bully_MVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bully_MVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230415164513_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
