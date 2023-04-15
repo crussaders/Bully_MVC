a1a9e6e0e6eba7b8d7dd40fdfb17cc7f5e87e431
Manish Kumar Pandey <896849@cognizant.com>
2023-04-16 01:07:36 +0530

    Design category page, used bootstrap icons, bootwatch

--- Bully_MVC/Controllers/CategoryController.cs
+++ Bully_MVC/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bully_MVC.Data;
+using Bully_MVC.Migrations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bully_MVC.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+           
+           List<Models.Category> objCategoryList = _db.Categories.ToList();
+           // _ = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
